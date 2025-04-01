e72cf7b218f0e34d38771549e26f87444e8901d7
KegangPeng <Kegangpeng1@gmail.com>
2025-04-01 11:59:25 -0700

    hid apikey

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 import express from "express"
 import axios from "axios"
 import { render } from "ejs";
+import dotenv from "dotenv";
+dotenv.config();
+
 // npm init -y
 // npm i express
 // npm i ejs
 // npm i axios
+// npm i dotenv
 
 const app = express();
 const port = 3000;
 app.use(express.static("public"))
 app.use(express.urlencoded({extended: true}));
 
-const weather_api_key = "a6b3e73741b18bd852114facf3dd4cc9";
+
+const weather_api_key = process.env.API_KEY;
+
 const url_zip_to_cor = "http://api.openweathermap.org/geo/1.0/zip?zip="
 const url_weather = "https://api.openweathermap.org/data/2.5/weather?";
 let zipcode;
@@ -24,13 +30,10 @@ app.get("/", async (req, res)=>{
 
 app.post("/getWeather", async (req,res)=>{
     zipcode = req.body.zipcode;
-    // console.log(zipcode)
     try{
         const lat_lon = await axios.get(url_zip_to_cor + zipcode + ",US&appid=" + weather_api_key);
         lat = lat_lon.data.lat;
         lon = lat_lon.data.lon;
-        // lat = 37.7806;
-        // lon = -122.2166;
         const result = await axios.get(`${url_weather}lat=${lat}&lon=${lon}&appid=${weather_api_key}&units=imperial`);
  
         res.render("index.ejs", {content:result.data});
